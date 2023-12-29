4f48ce30b47a0e8e5f9f6779c5bfada47b4a8c0d
devSid-os <siddjain341@gmail.com>
2023-12-29 15:48:33 +0530

    Comments added

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const score = document.getElementById("score");
 var currentScore = 0;
 var gameInterval = null;
 
+// Function to increment Score when game is in start mode
 function incrementScore(num) {
     const str = moundsDiv[num].children[0].src;
     if (str.includes("mole")) {
@@ -12,13 +13,15 @@ function incrementScore(num) {
     }
 }
 
+// Function to remove Mole
 function hideMole(num) {
     moundsDiv[num].children[0].src = "./img/mound.png";
 }
-
+// Function to show mole
 function showMole() {
     const randomNumber = Math.floor(Math.random() * moundsDiv.length);
     moundsDiv[randomNumber].children[0].src = "./img/mole.png";
+    // Hide mole After every 1 second and 100 miliseconds
     setTimeout(() => hideMole(randomNumber), 1100);
 }
 
@@ -32,7 +35,9 @@ function gameOver() {
 
 function startGame() {
     currentScore = 0;
+    // show mole after every 1 seconds and 100 miliseconds
     gameInterval = setInterval(showMole, 1100);
+    // End game after 20 seconds
     setTimeout(gameOver, 20000);
 }
 
